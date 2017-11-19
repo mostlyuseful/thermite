22754ea34ffac8137b1a6ba90be090331cb270a4
Maurice-Pascal Sonnemann <mpsonnemann@gmail.com>
2017-11-19 15:18:37 +0100

    Adapt dithering script to my printer's characteristics
    Brighter!

--- thermite/dither.sh
+++ thermite/dither.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-convert Sally_Ride_in_1984.jpg -resize 40% -dither Riemersma -colorspace gray -normalize -remap pattern:gray50 -normalize dithered.png
+convert Sally_Ride_in_1984.jpg -geometry 255 -brightness-contrast 30 -dither Riemersma -colorspace gray -normalize -remap pattern:gray50 -normalize dithered.png
